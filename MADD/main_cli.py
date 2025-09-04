21e9b9954ac4eee21134eb2fce69febdb2600cbb
alinazh <siberiangodness@mail.ru>
2025-09-04 14:17:14 +0300

    Upd config example

--- MADD/main_cli.py
+++ MADD/main_cli.py
@@ -8,20 +8,21 @@ if __name__ == "__main__":
     os.environ['DS_FROM_BINDINGDB'] = str(False)
     os.environ['DS_FROM_CHEMBL'] = str(False)
     os.environ['DS_FROM_USER'] = str(False)
+    # Add here path to dataset
     # os.environ['DS_FROM_USER'] = '/Users/alina/Desktop/ITMO/MADD-CoScientist/data_cyk_short.csv'
         
     graph = create_by_default_setup()
-    for step in graph.stream(
-        {
-            # "input": 'What models are available now?'
-            # "input": 'Download data from ChemBL for GSK with IC50 values.'
-            # "input": 'Start train model to predict PAINS, use my data.'
-            # "input": "Generate 2 molecules by 'PAINS_predictor'."
-            "input": "Check status of training for generative model with case 'PAINS_predictor'"
-
-        },
-        user_id="1",
-    ):
-        print(step)
+    while True:
+        for step in graph.stream(
+            {
+                # "input": 'What models are available now?'
+                # "input": 'Download data from ChemBL for GSK with IC50 values.'
+                # "input": 'Start train model to predict PAINS, use my data.'
+                # "input": "Generate 2 molecules by 'PAINS_predictor'."
+                # "input": "Check status of training for generative model with case 'PAINS_predictor'"
+                "input": input()
+            },
+            user_id="1",
+        ):
+            print(step)
         
-print('Ho')
